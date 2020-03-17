22eeb9390c90a6bc7b730aa1e5e3d93342757f1d
Ali <ali@gmail.com>
2020-03-17 12:20:38 +0500

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Components/Header/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <h1 className="header__heading">Corona Virus Tracker</h1>
+
+        <nav className="nav">
+          <NavLink
+            to="/"
+            className="nav__link"
+            activeClassName="nav__item--active"
+          >
+            <div className="nav__item">Home</div>
+          </NavLink>
+
+          <NavLink
+            to="/precautions"
+            className="nav__link"
+            activeClassName="nav__item--active"
+          >
+            <div className="nav__item">Precautions</div>
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
